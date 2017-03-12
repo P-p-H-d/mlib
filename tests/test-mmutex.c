30e30183172ce6ed9612b13529971a6b1b50d7fd
Patrick Pelissier <patrick.pelissier@gmail.com>
2017-03-12 18:58:25 +0100

    Add new test for mutex.

---
+++ tests/test-mmutex.c
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2017, Patrick Pelissier
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * + Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * + Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#include <stdio.h>
+
+#include "m-mutex.h"
+
+M_LOCK_DECL(global_lock);
+unsigned long long n = 0;
+
+static void *f(void *arg)
+{
+  (void)arg; // Unused
+  // Global lock to perform atomic addition.
+  M_LOCK(global_lock) {
+    n += 1;
+  }
+  return NULL;
+}
+
+static void test_global(void)
+{
+  M_THREAD_T idx_p[100];
+
+  n = 0;
+  for(int i = 0; i < 100; i++) {
+    M_THREAD_CREATE (idx_p[i], f, NULL);
+  }
+  for(int i = 0; i < 100;i++) {
+    M_THREAD_JOIN(idx_p[i]);
+  }
+  assert (n == 100);
+}
+
+int main(void)
+{
+  test_global();
+  exit(0);
+}
