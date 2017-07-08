30484f42919eb594c51a8c958102ac8b0997ea54
Patrick Pelissier <patrick.pelissier@gmail.com>
2017-07-08 15:14:01 +0200

    Factorisation.

--- bench/bench-libdynamic.c
+++ bench/bench-libdynamic.c
@@ -11,37 +11,7 @@
 
 #include "dynamic.h"
 
-unsigned long g_result;
-
-static unsigned long long
-cputime (void)
-{
-  struct rusage rus;
-  getrusage (0, &rus);
-  return rus.ru_utime.tv_sec * 1000000ULL + rus.ru_utime.tv_usec;
-}
-
-static unsigned int randValue = 0;
-static void rand_init(void)
-{
-  randValue = 0;
-}
-static unsigned int rand_get(void)
-{
-  randValue = randValue * 31421U + 6927U;
-  return randValue;
-}
-
-static void test_function(const char str[], size_t n, void (*func)(size_t))
-{
-  unsigned long long start, end;
-  //  (*func)(n);
-  start = cputime();
-  (*func)(n);
-  end = cputime();
-  end = (end - start) / 1000U;
-  printf ("%s %Lu ms for n = %lu\n", str, end, n);
-}
+#include "common.h"
 
 /********************************************************************************************/
 
