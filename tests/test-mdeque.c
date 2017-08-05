07c251e5ca524e808c5d0b60f8e59b64195ef5fa
Patrick Pelissier <Patrick.Pelissier@gmail.com>
2017-08-05 18:21:14 +0200

    Add test suite for deque

---
+++ tests/test-mdeque.c
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2017, Patrick Pelissier
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * + Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * + Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#include <stdio.h>
+#include <gmp.h> // For testing purpose only.
+
+#include "m-string.h"
+#include "m-deque.h"
+
+DEQUE_DEF(deque, int)
+
+static void test_ti1(int n)
+{
+  deque_t d;
+
+  deque_init(d);
+
+  for(int i =0; i < n ;i++) {
+    deque_push_back(d, i);
+    deque_push_front(d, i);
+  }
+  int s = 0;
+  for(int i =0; i < 2*n ;i++) {
+    int z;
+    deque_pop_front(&z, d);
+    s += z;
+  }
+  assert (s == n*(n-1) );
+  
+  deque_clear(d);
+}
+
+int main(void)
+{
+  test_ti1(10);
+  /*  test_ti1(100);
+  test_ti1(1000);
+  test_ti1(10000);*/
+}
