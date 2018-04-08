fe18009bc00cfb23e348a94572a95446d6acc8b6
Patrick Pelissier <Patrick.Pelissier@gmail.com>
2018-04-08 21:07:20 +0200

    Rewrite the loop so that the result is the same for dict and dict_oa.

--- bench/bench-mlib.c
+++ bench/bench-mlib.c
@@ -137,8 +137,9 @@ test_dict(unsigned long  n)
 #endif
   M_LET(dict, DICT_OPLIST(dict_ulong)) {
     for (size_t i = 0; i < n; i++) {
+      unsigned long value = rand_get();
       unsigned long key = rand_get();
-      dict_ulong_set_at(dict, key, rand_get() );
+      dict_ulong_set_at(dict, key, value);
     }
     rand_init();
     unsigned int s = 0;
