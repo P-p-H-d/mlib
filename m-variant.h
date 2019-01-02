8b3a4a802caa751ec7e45e4e2a216a832d5026fd
Patrick Pelissier <patrick.pelissier@gmail.com>
2019-01-02 18:12:07 +0100

    Happy new year!

--- m-variant.h
+++ m-variant.h
@@ -1,7 +1,7 @@
 /*
  * M*LIB - VARIANT module
  *
- * Copyright (c) 2017-2018, Patrick Pelissier
+ * Copyright (c) 2017-2019, Patrick Pelissier
  * All rights reserved.
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
