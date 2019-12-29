f14aca88086926204ebfdec1b7886d4b6c22367e
Patrick Pelissier <Patrick.Pelissier@gmail.com>
2019-12-29 23:51:30 +0100

    Port to new UDB2 protocol

--- bench/udb2/test-mlib.c
+++ bench/udb2/test-mlib.c
@@ -14,7 +14,7 @@ DICT_OA_DEF2(dict_oa_uint,
 	     unsigned int, M_OPEXTEND(M_DEFAULT_OPLIST, OOR_EQUAL(oor_equal_p), OOR_SET(oor_set M_IPTR)),
 	     unsigned int, M_DEFAULT_OPLIST)
 
-void test_int(uint32_t n, uint32_t x0)
+uint32_t test_int(uint32_t n, uint32_t x0)
 {
   uint32_t i, x, z = 0;
   dict_oa_uint_t h;
@@ -24,8 +24,10 @@ void test_int(uint32_t n, uint32_t x0)
     unsigned int key = get_key(n, x);
     unsigned int *ptr = dict_oa_uint_get_at(h, key);
     (*ptr)++;
-    z+= *ptr;
+    z += *ptr;
   }
   fprintf(stderr, "# unique keys: %d; checksum: %u\n", (int) dict_oa_uint_size(h), z);
+  z = dict_oa_uint_size(h);
   dict_oa_uint_clear(h);
+  return z;
 }
