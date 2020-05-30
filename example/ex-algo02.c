467acfdd1017776d77ea5cff741afb6d5aa5bcb9
Patrick Pelissier <patrick.pelissier@gmail.com>
2020-05-30 23:45:46 +0200

    Use of M_BOOL_OPLIST to avoid warnings.

--- example/ex-algo02.c
+++ example/ex-algo02.c
@@ -10,9 +10,9 @@ ARRAY_DEF(vector_int, int)
 ALGO_DEF(vector_int, vector_int_t)
 
 /* Define a dynamic array of bool */ 
-ARRAY_DEF(vector_bool, bool)
+ARRAY_DEF(vector_bool, bool, M_BOOL_OPLIST)
 /* Register the oplist of this dynamic array globally */
-#define M_OPL_vector_bool_t() ARRAY_OPLIST(vector_bool)
+#define M_OPL_vector_bool_t() ARRAY_OPLIST(vector_bool, M_BOOL_OPLIST)
 /* Define some standards algorithms on this dynamic array */
 ALGO_DEF(vector_bool, vector_bool_t)
 
