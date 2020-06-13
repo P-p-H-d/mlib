100af173282555acbe438f45898a10ceea8d5697
Patrick Pelissier <patrick.pelissier@gmail.com>
2020-06-13 10:44:20 +0200

    Make copyright notice explicit

--- tests/Make-check-cl.bat
+++ tests/Make-check-cl.bat
@@ -1,3 +1,25 @@
+REM Copyright (c) 2017-2020, Patrick Pelissier
+REM All rights reserved.
+REM 
+REM Redistribution and use in source and binary forms, with or without
+REM modification, are permitted provided that the following conditions are met:
+REM + Redistributions of source code must retain the above copyright
+REM   notice, this list of conditions and the following disclaimer.
+REM + Redistributions in binary form must reproduce the above copyright
+REM   notice, this list of conditions and the following disclaimer in the
+REM   documentation and/or other materials provided with the distribution.
+REM 
+REM THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+REM EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+REM WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+REM DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+REM DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+REM (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+REM LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+REM ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+REM (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+REM SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 REM Execute the test suite for Microsoft Visual Studio
 REM
 REM Load Micosoft Visual Studio environement file.
