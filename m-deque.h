d74f238a1447ae1ac172d2d1806d4ed5c2fcefd9
Vladislav Dmitrievich Turbanov <vladislav@turbanov.ru>
2020-07-05 13:33:59 +0300

    * Updates.

--- m-deque.h
+++ m-deque.h
@@ -31,8 +31,8 @@
    USAGE: DEQUE_DEF(name, type [, oplist_of_the_type]) */
 #define DEQUE_DEF(name, ...)                                            \
   DEQUEI_DEF_P1(M_IF_NARGS_EQ1(__VA_ARGS__)                             \
-                ((name, __VA_ARGS__, M_GLOBAL_OPLIST_OR_DEF(__VA_ARGS__)(), M_C(name,_t), M_C(name,_it_t), M_C(name, _node_t) ), \
-                 (name, __VA_ARGS__,                                        M_C(name,_t), M_C(name,_it_t), M_C(name, _node_t))))
+  ((name, __VA_ARGS__, M_GLOBAL_OPLIST_OR_DEF(__VA_ARGS__)(), M_C(name,_t), M_C(name,_it_t), M_C(name, _node_t) ), \
+   (name, __VA_ARGS__,                                        M_C(name,_t), M_C(name,_it_t), M_C(name, _node_t))))
 
 
 /* Define the oplist of a deque of a type.
