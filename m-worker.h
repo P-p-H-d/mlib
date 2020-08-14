379e0be2112d0f2bad4467fdce0609acb7646051
Patrick Pelissier <patrick.pelissier@gmail.com>
2020-08-14 23:41:30 +0200

    Use of new macro to define internal types not exported.

--- m-worker.h
+++ m-worker.h
@@ -142,13 +142,13 @@ typedef struct worker_thread_s {
 } worker_thread_ct;
 
 /* Definition of the queue that will record the work orders */
-BUFFER_DEF(worker_queue, worker_order_ct, 0, 
+BUFFER_DEF_AS(worker_queue, m_worker_queue_ct, worker_order_ct, 0, 
            BUFFER_QUEUE|BUFFER_UNBLOCKING_PUSH|BUFFER_BLOCKING_POP|BUFFER_THREAD_SAFE|BUFFER_DEFERRED_POP, WORKER_OPLIST)
 
 /* Definition the global pool of workers */
 typedef struct worker_s {
   /* The work order queue */
-  worker_queue_t queue_g;
+  m_worker_queue_ct queue_g;
 
   /* The table of available workers */
   worker_thread_ct *worker;
