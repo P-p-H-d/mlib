9e8ad9bfd9dd50286c8c347ccf06fd1af53f16ad
Patrick Pelissier <patrick.pelissier@gmail.com>
2022-05-09 22:48:56 +0200

    Fix format

--- m-variant.h
+++ m-variant.h
@@ -105,7 +105,7 @@
   M_VAR1ANT_DEFINE_CLEAR(name, __VA_ARGS__)                                   \
   M_VAR1ANT_DEFINE_INIT_SET(name, __VA_ARGS__)                                \
   M_VAR1ANT_DEFINE_SET(name, __VA_ARGS__)                                     \
-  M_VAR1ANT_DEFINE_EMPLACE(name, __VA_ARGS__) \
+  M_VAR1ANT_DEFINE_EMPLACE(name, __VA_ARGS__)                                 \
   M_VAR1ANT_DEFINE_TEST_P(name, __VA_ARGS__)                                  \
   M_VAR1ANT_IF_ALL(INIT, __VA_ARGS__)                                         \
   (M_VAR1ANT_DEFINE_INIT_FIELD(name, __VA_ARGS__),)                           \
@@ -393,12 +393,12 @@
 #define M_VAR1ANT_MAP3_ALT(f, d, ...)   M_REDUCE3(f, M_VAR1ANT_MAP3_ALT_ID, d, __VA_ARGS__)
 #define M_VAR1ANT_MAP3_ALT_ID(a, b)     a b
 
-#define M_VAR1ANT_DEFINE_EMPLACE_FUNC(name, num, a)                      \
+#define M_VAR1ANT_DEFINE_EMPLACE_FUNC(name, num, a)                           \
   M_EMPLACE_QUEUE_DEF( (name, M_VAR1ANT_GET_FIELD a), M_C(name,_ct), M_C3(name, _init_emplace_, M_VAR1ANT_GET_FIELD a), M_VAR1ANT_GET_OPLIST a, M_VAR1ANT_DEFINE_INIT_EMPLACE_DEF) \
   M_EMPLACE_QUEUE_DEF( (name, M_VAR1ANT_GET_FIELD a), M_C(name,_ct), M_C3(name, _emplace_, M_VAR1ANT_GET_FIELD a), M_VAR1ANT_GET_OPLIST a, M_VAR1ANT_DEFINE_EMPLACE_DEF)
 
 #define M_VAR1ANT_DEFINE_INIT_EMPLACE_DEF(name, name_t, function_name, oplist, init_func, exp_emplace_type) \
-static inline void                                                      \
+static inline void                                                            \
   function_name(name_t my                                                     \
                 M_EMPLACE_LIST_TYPE_VAR(ab, exp_emplace_type) )               \
   {                                                                           \
@@ -411,8 +411,8 @@ static inline void                                                      \
   function_name(name_t my                                                     \
                 M_EMPLACE_LIST_TYPE_VAR(ab, exp_emplace_type) )               \
   {                                                                           \
-    /* No optimization done */                                          \
-    M_C(M_PAIR_1 name, _clear)(my);                                     \
+    /* No optimization done */                                                \
+    M_C(M_PAIR_1 name, _clear)(my);                                           \
     my->type = M_C4(M_PAIR_1 name, _, M_PAIR_2 name, _value);                 \
     M_EMPLACE_CALL_FUNC(ab, init_func, oplist, my -> value. M_PAIR_2 name, exp_emplace_type); \
   }                                                                           \
