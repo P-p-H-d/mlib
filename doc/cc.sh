c3f6e3c5f03887458810374028bd50ff287601f8
Patrick Pelissier <patrick.pelissier@gmail.com>
2023-03-19 00:29:51 +0100

    Add trivial script to separate preproc of compilation for debug

---
+++ doc/cc.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+mkdir -p /tmp/prepro-$USER
+name=/tmp/prepro-$USER/$$.c
+param1=""
+param2=""
+while test $# -gt 0 ; do
+    case $1 in
+        -c) param2="$param2 -c" ;;
+        *\.c)  param1="$param1 $1" ;;
+        *) param1="$param1 $1" ; param2="$param2 $1" ;;
+     esac
+    shift
+done
+cc $param1 -E |grep -v '^#' > ${name}      \
+    && perl -i -e  's/;/;\n/g' ${name}    \
+    && cc $param2 ${name}
