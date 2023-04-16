58a7a7c214fb89b5022b3a9e62eddfb78de7b49d
Patrick Pelissier <patrick.pelissier@gmail.com>
2023-04-16 19:15:54 +0200

    Add gene TRY

---
+++ tests/tgen-try.c
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2017-2023, Patrick Pelissier
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * + Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * + Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "m-try.h"
+
+typedef struct testobj_s {
+  unsigned int n;
+  unsigned int a;
+  bool         allocated;
+  unsigned int *ptr;
+} testobj_t[1];
+
+extern void testobj_init(testobj_t z);
+extern void testobj_clear(testobj_t z);
+#define M_OPL_testobj_t() (INIT(testobj_init), CLEAR(testobj_clear), TYPE(testobj_t))
+
+void test2(void)
+{
+  M_TRY(test1) {
+    M_LET(obj, testobj_t) {
+      f(obj);
+    }
+  } M_CATCH(test1, 0) {
+    assert(0);
+  }
+}
