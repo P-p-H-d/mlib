a043c379c0477ed57fafeb51421add85f65fc532
Patrick Pelissier <Patrick.Pelissier@gmail.com>
2023-05-18 01:08:11 +0200

    Update user code as per new liberty

--- example/exm-header.h
+++ example/exm-header.h
@@ -2,9 +2,7 @@
 #define MY_HEADER
 
 // Request M*LIB to not inline functions if not the library source
-#ifndef M_USE_EXTERN_DEF
 #define M_USE_EXTERN_DECL
-#endif
 #include "m-string.h"
 #include "m-array.h"
 
--- example/exm-lib.c
+++ example/exm-lib.c
@@ -1,4 +1,5 @@
 // Request M*LIB to define functions
 // This include both m-string functions and array_str templated functions
+// Defining M_USE_EXTERN_DEF override M_USE_EXTERN_DECL definition
 #define M_USE_EXTERN_DEF
 #include "exm-header.h"
