8902a752f5765749053f91097689a9ad56526312
Patrick Pelissier <Patrick.Pelissier@gmail.com>
2023-12-31 13:02:42 +0100

    Migrate to V0.7.2

--- m-core.h
+++ m-core.h
@@ -71,7 +71,7 @@
 /* Define M*LIB version */
 #define M_CORE_VERSION_MAJOR 0
 #define M_CORE_VERSION_MINOR 7
-#define M_CORE_VERSION_PATCHLEVEL 1
+#define M_CORE_VERSION_PATCHLEVEL 2
 
 /* M_ASSUME is equivalent to M_ASSERT, but gives hints to compiler
    about how to optimize the code if NDEBUG is defined.
