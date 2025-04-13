cc97aa887f7d8f458b179b7cd07bc85f73bb5a39
Patrick Pelissier <patrick.pelissier@gmail.com>
2025-04-13 16:11:46 +0200

    Fix reference

--- tests/test-mgeneric.c
+++ tests/test-mgeneric.c
@@ -30,7 +30,7 @@
 
 // Generic is not supported if not C11
 // TCC has some issues in the preprocessing.
-// See https://lists.nongnu.org/archive/html/tinycc-devel/2025-12/msg00004.html
+// See https://lists.nongnu.org/archive/html/tinycc-devel/2023-12/msg00004.html
 #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L && !defined(__TINYC__)
 
 ARRAY_DEF(array_int, int)
