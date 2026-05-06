fc94367acd402f7af9c652563dbdda2c603ede91
Patrick Pelissier <patrick.pelissier@gmail.com>
2026-05-06 23:03:07 +0200

    Fix compiler script

--- doc/cc.sh
+++ doc/cc.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-mkdir -p /tmp/prepro-$USER
 name=/tmp/prepro-$USER/$$.c
 param1=""
 param2=""
@@ -7,11 +6,14 @@ while test $# -gt 0 ; do
     case $1 in
         -c) param2="$param2 -c" ;;
         -o) param2="$param2 -o $2" ; shift ;;
-        *\.c)  param1="$param1 $1" ;;
+        *\.c)  param1="$param1 $1" ; name=/tmp/prepro-$USER/$1 ;;
         *) param1="$param1 $1" ; param2="$param2 $1" ;;
      esac
     shift
 done
-cc $param1 -E |grep -v '^#' > ${name}      \
-    && perl -pi -e  's/\;/\;\n/g' ${name}    \
+d=$(dirname $name)
+mkdir -p $d
+echo "Preprocessing to $name"
+cc $param1 -E |grep -v '^# ' > ${name}      \
+    && clang-format -i ${name}              \
     && cc $param2 ${name}
